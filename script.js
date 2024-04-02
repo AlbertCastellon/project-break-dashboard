d1486b9e7f69038db6a39152d5b3a81a484e93d8
AlbertCastellon <albertkstyon@gmail.com>
2024-04-02 22:55:40 +0200

    reloj y frase final

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ const getDate = () => {
     let month = ('0' + (infoDate.getMonth() + 1)).slice(-2);
     let year = infoDate.getFullYear();
     let time = infoDate.toLocaleTimeString();
-    fraseHora(time)
+    let frase = fraseHora(time)
     date.innerHTML = `${day}/${month}/${year}`;
-    clock.innerHTML = time
+    clock.innerHTML = time+ ' ' + frase
     
 }
 const changeBackground = () => {
@@ -19,13 +19,31 @@ const changeBackground = () => {
     document.body.style.backgroundImage = url
     }
 const fraseHora = (hora) => {
-    let arr = hora.split(':').map(element => parseInt(element))
-    if((arr[0] === 0 && arr[1] > 0)||( 0 <= arr[0] < 7)||arr[0] === 7 && arr[1] === 0)
+    let arr = hora.split(':')
+    arr.pop()
+    let horaDec = parseInt(arr.join('.'))
+    
+    if(0 < horaDec <= 7){
+        frase = 'Es hora de descansar. Apaga y sigue mañana'
+    }else if(7 < horaDec <= 12){
+        frase = 'Buenos días, desayuna fuerte y a darle al código'
+    }else if(12 < horaDec <= 14){
+        frase = 'Echa un rato más pero no olvides comer'
+    }else if(14 < horaDec <= 16){
+        frase = 'Espero que hayas comido'
+    }else if(16 < horaDec <= 18){
+        frase = 'Buenas tardes, el último empujón'
+    }else if(18 < horaDec <= 22){
+        frase = 'Esto ya son horas extras, ... piensa en parar pronto'
+    }else if(horaDec > 22 || horaDec === 0){
+        frase = 'Buenas noches, es hora de pensar en parar y descansar'
+    }
+    return frase
 }
-/*setInterval(() => {
+changeBackground()
+setInterval(() => {
     getDate()
-}, 1000)*/
+}, 1000)
 setInterval(() => {
     changeBackground()
 }, 15000)
-getDate()
